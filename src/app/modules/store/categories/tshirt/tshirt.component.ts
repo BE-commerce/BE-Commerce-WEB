669112406038861f9007bcd3d548c74712dc15a5
Brkesnglu <burakesngluuu@gmail.com>
2022-09-11 23:21:32 +0300

    product-list

--- src/app/modules/store/categories/tshirt/tshirt.component.ts
+++ src/app/modules/store/categories/tshirt/tshirt.component.ts
@@ -9,6 +9,16 @@ export class TshirtComponent implements OnInit {
 
   constructor() { }
 
+  products: any[] = [
+    { id: 1, image: 'https://www.kaft.com/static/images/cache/1200/tisort_theland_21108_1200_1200.jpg?cacheID=1644829419000', name: 'The Land', price: 240, currency: 'TL', rating: [1, 2, 3] },
+    { id: 2, image: 'https://www.kaft.com/static/images/cache/1200/tisort_ravera_21094_1200_1200.jpg?cacheID=1657024801000', name: 'The Home', price: 270, currency: 'TL', rating: [1, 2] },
+    { id: 3, image: 'https://www.kaft.com/static/images/cache/1200/tisort_time_19638_1200_1200.jpg?cacheID=1659413605000', name: 'The Car', price: 270, currency: 'TL', rating: [1, 2, 3, 4, 5] },
+    { id: 4, image: 'https://www.kaft.com/static/images/cache/1200/tisort_yuvica_20996_1200_1200.jpg?cacheID=1644829418000', name: 'The Table', price: 250, currency: 'TL', rating: [1, 2, 3] },
+    { id: 5, image: 'https://www.kaft.com/static/images/cache/1200/tisort_cosmos_18208_1200_1200.jpg?cacheID=1634184805000', name: 'The Universe', price: 190, currency: 'TL', rating: [1, 2, 3, 4] },
+    { id: 6, image: 'https://www.kaft.com/static/images/cache/1200/tisort_laugar_21045_1200_1200.jpg?cacheID=1654185714000', name: 'The World', price: 230, currency: 'TL', rating: [1, 2] },
+    { id: 7, image: 'https://www.kaft.com/static/images/cache/1200/tisort_astrok_18067_1200_1200.jpg?cacheID=1651236995000', name: 'The Humanity', price: 250, currency: 'TL', rating: [1, 2, 3] }
+  ]
+
   ngOnInit() {
   }
 
